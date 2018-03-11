c4500ef41e4edb83661d5857f2e62497134d06b0
torss <37229901+torss@users.noreply.github.com>
2018-03-11 02:14:37 +0100

    Add rudimentary CMake support

--- FastNoiseSIMD/FastNoiseSIMD.h
+++ FastNoiseSIMD/FastNoiseSIMD.h
@@ -34,29 +34,29 @@
 #if defined(__arm__) || defined(__aarch64__)
 #define FN_ARM
 //#define FN_IOS
-#define FN_COMPILE_NEON
+#cmakedefine FN_COMPILE_NEON
 #else
 
 // Comment out lines to not compile for certain instruction sets
-#define FN_COMPILE_SSE2
-#define FN_COMPILE_SSE41
+#cmakedefine FN_COMPILE_SSE2
+#cmakedefine FN_COMPILE_SSE41
 
 // To compile AVX2 set C++ code generation to use /arch:AVX(2) on FastNoiseSIMD_avx2.cpp
 // Note: This does not break support for pre AVX CPUs, AVX code is only run if support is detected
-#define FN_COMPILE_AVX2
+#cmakedefine FN_COMPILE_AVX2
 
 // Only the latest compilers will support this
-//#define FN_COMPILE_AVX512
+#cmakedefine FN_COMPILE_AVX512
 
 // Using FMA instructions with AVX(51)2/NEON provides a small performance increase but can cause 
 // minute variations in noise output compared to other SIMD levels due to higher calculation precision
 // Intel compiler will always generate FMA instructions, use /Qfma- or -no-fma to disable
-#define FN_USE_FMA
+#cmakedefine FN_USE_FMA
 #endif
 
 // Using aligned sets of memory for float arrays allows faster storing of SIMD data
 // Comment out to allow unaligned float arrays to be used as sets
-#define FN_ALIGNED_SETS
+#cmakedefine FN_ALIGNED_SETS
 
 // SSE2/NEON support is guaranteed on 64bit CPUs so no fallback is needed
 #if !(defined(_WIN64) || defined(__x86_64__) || defined(__ppc64__) || defined(__aarch64__) || defined(FN_IOS)) || defined(_DEBUG)
